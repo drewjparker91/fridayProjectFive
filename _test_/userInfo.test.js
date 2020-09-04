9b32f53209bcb041edd4bcda2169c57b9b3f9ded
drewparker <drewjparker91@gmail.com>
2020-09-04 10:35:54 -0700

    DRY up code by creating beforeEach blocks for tests

--- _test_/userInfo.test.js
+++ _test_/userInfo.test.js
@@ -2,25 +2,30 @@ import {UserInfo} from '../src/js/UserInfo.js';
 import { TestScheduler } from 'jest'
 
 describe('UserInfo', () => {
+    let reusableUserInfo;
+
+    beforeEach (() => {
+      reusableUserInfo = new UserInfo(20);
+    });
 
     test('should return age on Mercury based on user inputted age', () => {
-    const userInfo = new UserInfo(20)
-    expect(userInfo.mercuryAge(userInfo.age)).toEqual(83.3333);
+      // const userInfo = new UserInfo(20)
+    expect(reusableUserInfo.mercuryAge(reusableUserInfo.age)).toEqual(83.3333);
     })
 
     test('should return age on Venus based on user inputted age', () => {
-      const userInfo = new UserInfo(20)
-      expect(userInfo.venusAge(userInfo.age)).toEqual(32.2581);
+      // const userInfo = new UserInfo(20)
+      expect(reusableUserInfo.venusAge(reusableUserInfo.age)).toEqual(32.2581);
     })
 
     test('should return age on Mars based on user inputted age', () => {
-      const userInfo = new UserInfo(20)
-      expect(userInfo.marsAge(userInfo.age)).toEqual(10.6383);
+      // const userInfo = new UserInfo(20)
+      expect(reusableUserInfo.marsAge(reusableUserInfo.age)).toEqual(10.6383);
     })
 
     test('should return age on Jupiter based on user inputted age', () => {
-      const userInfo = new UserInfo(20)
-      expect(userInfo.jupiterAge(userInfo.age)).toEqual(1.6863);
+      // const userInfo = new UserInfo(20)
+      expect(reusableUserInfo.jupiterAge(reusableUserInfo.age)).toEqual(1.6863);
     })
     
 })
