fd4f4de20b36d93ebab66bbba48dbb1eaa2cd9ed
drewparker <drewjparker91@gmail.com>
2020-09-04 10:09:45 -0700

    good fail for mercuryAge test

--- _test_/userInfo.test.js
+++ _test_/userInfo.test.js
@@ -1,9 +1,10 @@
-import {UserInfo} from '../src/js/UserInfo.js'
+import {UserInfo} from '../src/js/UserInfo.js';
 import { TestScheduler } from 'jest'
 
 describe('UserInfo', () => {
 
-  test('should calculate age on mercury based on user inputted age')
-  const userInfo = new UserInfo(20)
-  expect(userInfo.mercuryYears(userInfo.age)).toEqual(83.333);
+    test('should return age on mercury based on user inputted age', () => {
+    const userInfo = new UserInfo(20)
+    expect(userInfo.mercuryAge(userInfo.age)).toEqual(83.333);
+    })
 })
--- src/js/userInfo.js
+++ src/js/userInfo.js
@@ -1,13 +1,29 @@
-export default class UserInfo {
-  constructor(age, sex, exercise) {
+export class UserInfo{
+  constructor(age, sex, exercise, lifeExpectancy) {
     this.age = age;
     this.sex = sex;
     this.exercise = exercise;
-    this.lifeExpectancy = 0
-
+    this.lifeExpectancy = lifeExpectancy
+  }  
   mercuryAge(years) {
-    let mercuryYears = parseFloat((year/.24).toFixed(4))
-    return mercuryYears
+    let mercuryAge = parseFloat((years/.24).toFixed(4))
+    return mercuryAge
   }
+
+  // venusAge(years) {
+  //   let venusAge = parseFloat((years/.62).toFixed(4))
+  //   return venusAge
+  // }
+
+  // marsAge(years) {
+  //   let marsAge = parseFloat((years/1.88).toFixed(4))
+  //   return marsAge
+  // }
+
+  // jupiterAge(years) {
+  //   let jupiterAge = parseFloat((year/11.86).toFixed(4))
+  //   return jupiterAge
+  // }
+
   
 } 
