da58cb1d620b673aab737d1f15aa1f621c2f2984
drewparker <drewjparker91@gmail.com>
2020-09-04 16:09:31 -0700

    DRY code in test.js for determining age on a different planet

--- _test_/userInfo.test.js
+++ _test_/userInfo.test.js
@@ -7,26 +7,33 @@ describe('UserInfo', () => {
     beforeEach (() => {
       reusableUserInfo = new UserInfo(20, 80);
     });
-
-    test('should return age on Mercury based on user inputted age', () => {
-      // const userInfo = new UserInfo(20)
-    expect(reusableUserInfo.mercuryAge(reusableUserInfo.age)).toEqual(83.3333);
-    })
-
-    test('should return age on Venus based on user inputted age', () => {
-      // const userInfo = new UserInfo(20)
+    test('should return age on a given planet based on user inputted age', () => {
+      expect(reusableUserInfo.mercuryAge(reusableUserInfo.age)).toEqual(83.3333);
       expect(reusableUserInfo.venusAge(reusableUserInfo.age)).toEqual(32.2581);
-    })
-
-    test('should return age on Mars based on user inputted age', () => {
-      // const userInfo = new UserInfo(20)
       expect(reusableUserInfo.marsAge(reusableUserInfo.age)).toEqual(10.6383);
-    })
-
-    test('should return age on Jupiter based on user inputted age', () => {
-      // const userInfo = new UserInfo(20)
       expect(reusableUserInfo.jupiterAge(reusableUserInfo.age)).toEqual(1.6863);
     })
+    
+    
+    // test('should return age on Mercury based on user inputted age', () => {
+    //   // const userInfo = new UserInfo(20)
+    // expect(reusableUserInfo.mercuryAge(reusableUserInfo.age)).toEqual(83.3333);
+    // })
+
+    // test('should return age on Venus based on user inputted age', () => {
+    //   // const userInfo = new UserInfo(20)
+    //   expect(reusableUserInfo.venusAge(reusableUserInfo.age)).toEqual(32.2581);
+    // })
+
+    // test('should return age on Mars based on user inputted age', () => {
+    //   // const userInfo = new UserInfo(20)
+    //   expect(reusableUserInfo.marsAge(reusableUserInfo.age)).toEqual(10.6383);
+    // })
+
+    // test('should return age on Jupiter based on user inputted age', () => {
+    //   // const userInfo = new UserInfo(20)
+    //   expect(reusableUserInfo.jupiterAge(reusableUserInfo.age)).toEqual(1.6863);
+    // })
     
     test ('should return years left remaining on a planet based on user inputted life expectancy', () => {
       if (reusableUserInfo.age < reusableUserInfo.lifeExpectancy)
